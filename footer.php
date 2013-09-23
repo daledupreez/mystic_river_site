bb983c49ffb2619529a33a2b297e83521d1bfefd
Dale du Preez <daledupreez@gmail.com>
2013-09-22 22:55:17 -0400

    Add sponsors to theme
    Add sponsors to footer and initial sponsor images

--- footer.php
+++ footer.php
@@ -15,12 +15,28 @@
 	<footer id="colophon" role="contentinfo">
 
 			<?php
+				$imageDir = esc_url( get_stylesheet_directory_uri() . "/images/" );
 				/* A sidebar in the footer? Yep. You can can customize
 				 * your footer with three columns of widgets.
 				 */
 				if ( ! is_404() )
 					get_sidebar( 'footer' );
 			?>
+
+			<div id="sponsors">
+				<a id="link_coors" class="sponsor_link" href="http://www.coorslight.com" target="_blank" alt="Coors Light">
+					<img class="sponsor_logo" src="<?php echo $imageDir; ?>coors_light_logo.png" />
+				</a>
+				<a id="link_harp" class="sponsor_link" href="http://www.harpboston.com" target="_blank" alt="The Harp">
+					<img class="sponsor_logo" src="<?php echo $imageDir; ?>harp_logo.png" />
+				</a>
+				<a id="link_llanllyr" class="sponsor_link" href="http://www.llanllyrwater.com/" target="_blank" alt="Llanllyr Source Water">
+					<img class="sponsor_logo" src="<?php echo $imageDir; ?>llanllyr_source_logo.png" />
+				</a>
+				<a id="link_ruggers" class="sponsor_link" href="http://www.ruggers.com/" target="_blank" alt="Ruggers">
+					<img class="sponsor_logo" src="<?php echo $imageDir; ?>ruggers_logo.png" />
+				</a>
+			</div>
 
 			<div id="site-generator">
 				<a href="<?php echo esc_url( get_admin_url() ); ?>" title="Log in to Admin pages">Admin</a>
